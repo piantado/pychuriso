a6bc31fec8fbf2bdbe323c28e36fb17340d6a9d9
Jenna Register <jennaregister@dhcp-10-4-30-183.wireless.rochester.edu>
2016-11-15 12:34:48 -0500

    added unit tests to test reduction code

---
+++ reductionTest.py
@@ -0,0 +1,14 @@
+import unittest
+from reduction import reduce_combinator
+
+class ReductionTest(unittest.TestCase):
+    def runTest(self):
+        #dictionary tests contains combinator strings (key) and expected reduction (value)
+        tests = {'.....SKKKxy':'x','..Kxy':'x','....E.....SKKKxy..Kxyab':'a','....E.....KxySxyzab':'b','....E.......KxySxyzKxySxyz':'Sxyz','..Txy':'yx','.Mx':'xx','...Bxyz':'.x.yz','...Sxyz':'..xz.yz','..Wxy':'xyy','.Ix':'x'}
+        print "# Testing reduction"
+        for k,v in tests.iteritems():
+            #assert that reduce_combinator returns the expected value
+            self.assertEqual(reduce_combinator(k),v)
+
+if __name__ == '__main__':
+    unittest.main()
