002f26a1b3e09992b60677ee98bedb3d078517b7
Jenna Register <jennaregister@dhcp-10-5-7-7.wireless.rochester.edu>
2017-01-18 10:48:50 -0500

    adding more combinator bases to run

--- Projects/RelationInduction/Experiment1/plotModel.R
+++ Projects/RelationInduction/Experiment1/plotModel.R
@@ -14,13 +14,15 @@ for(f in list.files("model-outputs/", full.names=TRUE)) {
 
     ## add up prob over hypotheses (rows), then renormalize within generalization,condition
     ag <- d %>% group_by(generalization, condition, answer) %>% summarise(sump=sum(p)) %>% group_by(generalization, condition) %>% mutate(probability=normalize(sump)) %>% as.data.frame()
-
-
-    plt <- ggplot(ag, aes(x=answer, y=probability)) +
-            geom_point() + 
-            facet_grid(generalization ~ condition)
-            
-    ggsave(paste(d$basis[1],".pdf",sep=""), plt)
+    View(ag)
+   
+    plt <- ggplot(ag, aes(x=answer, y=probability, fill=as.factor(condition))) +
+            geom_bar(stat="identity") + 
+            facet_grid(generalization ~ condition)+
+            ggtitle(f)
+            #ylim(0,1)
+    plt     
+    ggsave(paste("model_plots/",d$basis[1],".pdf",sep=""), plt)
 }
 
 
