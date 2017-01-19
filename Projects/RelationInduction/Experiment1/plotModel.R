94b696ac167662783262058f2c43670e1023d5c7
Jenna from the block! <jregiste@u.rochester.edu>
2017-01-19 15:06:11 -0500

    fixing my model R script

--- Projects/RelationInduction/Experiment1/plotModel.R
+++ Projects/RelationInduction/Experiment1/plotModel.R
@@ -5,6 +5,7 @@ library(dplyr)
 
 
 for(f in list.files("model-outputs/", full.names=TRUE)) {
+    View(file.info(f)$size)
     d <- read.table(f, header=TRUE)
 
     # what if our probability was proportional to the runtime
@@ -15,14 +16,17 @@ for(f in list.files("model-outputs/", full.names=TRUE)) {
     ## add up prob over hypotheses (rows), then renormalize within generalization,condition
     ag <- d %>% group_by(generalization, condition, answer) %>% summarise(sump=sum(p)) %>% group_by(generalization, condition) %>% mutate(probability=normalize(sump)) %>% as.data.frame()
     View(ag)
-   
+    name <- gsub("model-test//","",f)
+    name <-gsub(".txt","",name)
     plt <- ggplot(ag, aes(x=answer, y=probability, fill=as.factor(condition))) +
             geom_bar(stat="identity") + 
             facet_grid(generalization ~ condition)+
-            ggtitle(f)
+            ggtitle(name)+
+            xlab("Response")+
+            ylab("Probability Under the Model")
             #ylim(0,1)
     plt     
-    ggsave(paste("model_plots/",d$basis[1],".pdf",sep=""), plt)
+    ggsave(paste("model-plots2/",d$basis[1],".pdf",sep=""), plt)
 }
 
 
