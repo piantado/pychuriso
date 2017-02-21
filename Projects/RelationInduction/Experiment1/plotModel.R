6635c2cdc9f92ab9f4662445a21978d86a86682e
piantado <spiantado@gmail.com>
2017-02-21 12:36:41 -0500

    unknown updates

--- Projects/RelationInduction/Experiment1/plotModel.R
+++ Projects/RelationInduction/Experiment1/plotModel.R
@@ -1,5 +1,6 @@
 ## TODO:
-## Human needs to output counts N
+## When we compute the permuted samples, we should remove the samples that the model was given, probably? These are penalized in randomized samples but are not really penalized for the real sample since there is fitting. 
+
 
 library(ggplot2)
 library(plyr)
@@ -14,10 +15,10 @@ human$condition <- as.factor(human$condition)
 human$generalization <- as.factor(human$generalization)
 
 #what does the human data look like?
-humplt = ggplot(human, aes(answer, human.probability, fill=as.factor(condition)))+
-  geom_bar(stat="identity")+
-  facet_grid(~generalization~condition)
-humplt
+# humplt = ggplot(human, aes(answer, human.probability, fill=as.factor(condition)))+
+#   geom_bar(stat="identity")+
+#   facet_grid(~generalization~condition)
+# humplt
 
 #put the human frame into the form of the model frame
 hp <- human %>% arrange(generalization) %>% as.data.frame()
@@ -26,20 +27,15 @@ hp$x <- NULL
 #initialize the correlations table
 correlations = data.frame(Basis=character(),Correlation=double(),Kendall=double())
 
-#inititalize the faceted correlations table
-#faceted_cors = data.frame(Basis=character(), Condition = integer(), Generalization = character(),Correlation = double())
-faceted_cors = NULL
-#go through all the model outputs
-
 normalize <- function(x) { x/sum(x, na.rm=TRUE)} # fix normalization!
             
-
-D <- NULL # summary table 
+D <- NULL # summary table for the true data
+P <- NULL # table of permuted versions
 # for(f in c("model-outputs/SKHIE.txt")){#list.files("model-outputs/", full.names=TRUE)) {
 for(f in list.files("model-outputs/", full.names=TRUE)) {
-for(param in c(0.01, 0.1, 1.0, 10.0, 20.0, 50.0)) {
+for(param in c(0.01, 0.1, 1.0, 10.0, 20.0, 50.0, 100.0)) {
 
-            d <- read.table(f, header=TRUE)
+            d <- read.table(gzfile(f), header=TRUE)
             if(nrow(d)==0) { next } 
             d$condition <- as.factor(d$condition)
             
@@ -63,7 +59,8 @@ for(param in c(0.01, 0.1, 1.0, 10.0, 20.0, 50.0)) {
             
             # Merge together
             m <- merge(hp, ag, by=c("generalization", "condition", "answer"), all.x=TRUE, all.y=TRUE)
-            m[is.na(m$human.probability),"human.probability"] <- 0 # fix the human zeros
+            m[is.na(m$human.probability),"human.probability"] <- 0 # fix the human zeros -- should not matter since these should have zero count
+            m[is.na(m$model.probability),"model.probability"] <- 0 # fix the model zeros
             
             # Let's make a plot with both since its easier to see
             plt <- ggplot(m, aes(x=answer, y=human.probability, fill=condition)) +
@@ -81,154 +78,43 @@ for(param in c(0.01, 0.1, 1.0, 10.0, 20.0, 50.0)) {
             D <- rbind(D, data.frame(basis=d$basis[1], 
                                      cor=cor.test(m$human.probability, m$model.probability)$estimate,
                                      mse=mean( (m$human.probability-m$model.probability)**2.0, na.rm=TRUE ),
-                                     ll=sum(m$human.count*log(m$model.probability*0.99 + 0.01/5.0)),
+                                     ll=sum(m$human.count*log(m$model.probability*0.99 + 0.01/5.0), na.rm=TRUE),
                                      param=param,
                                      missing= sum(is.na(m$human.probability-m$model.probability))
                                      ))
-                                   
+                           
+            # since the data is all loaded, do the permuted version
+            # we'll make a column "p" so we can pull out p=1, p=2, etc. to get the different permutations
+            # of the entire dataset
+            for(n in 1:300) {
+                idx <- sample(1:nrow(m)) # shuffle prob and count the same (keeping them paired)
+                m$human.probability <- m$human.probability[idx]
+                m$human.count <- m$human.count[idx]
+                
+                # now save the permuted data in P
+                P <- rbind(P, data.frame(n=n,  # what permutation are we on?
+                                basis=d$basis[1], 
+                                cor=cor.test(m$human.probability, m$model.probability)$estimate,
+                                mse=mean( (m$human.probability-m$model.probability)**2.0, na.rm=TRUE ),
+                                ll=sum(m$human.count*log(m$model.probability*0.99 + 0.01/5.0), na.rm=TRUE),
+                                param=param,
+                                missing= sum(is.na(m$human.probability-m$model.probability))
+                                ))
+            }
       }
       
-      print(head(D[order(D$mse),]))
+      print(head(D[order(-D$ll),]))
       
 }
   
+## Now if we want we can process the permutations
+## Compute the ll for each permutation (indexed by n)
+maxes <- P %>% group_by(n) %>% summarise(ll=max(ll), cor=max(cor), mse=min(mse)) %>% as.data.frame()
 
-# 
-# write.csv(correlations,"correlations.csv")
-# write.csv(faceted_cors,"condition_cors.csv")
-# 
-# 
-# #plot the overall correlations
-# correlations = read.csv("correlations.csv")
-# correlations$Legend = "Lower than .4"
-# correlations$Legend[correlations$Kendall>.4]="Higher than .4"
-# 
-# correlations =correlations[order(-correlations$Kendall),]
-# correlations = correlations[1:100,]
-# #View(correlations)
-# 
-# plt = ggplot(correlations,aes(Basis,Kendall,fill=Legend))+
-#   geom_bar(stat="identity")+
-#   theme(axis.text.x = element_text(angle=60, hjust=1))
-#   
-# plt
-# ggsave("Correlations.pdf" ,plt,width=10)
-# 
-# #plot the correlations by condition
-# cond_correlations = read.csv("condition_cors.csv")
-# cond_correlations =cond_correlations[order(-cond_correlations$cor),]
-# 
-# #generate plots for each condition (plot only bases that do better than .7 of the mean correlation)
-# for(i in 0:4){
-#   print(i)
-#  #name = paste("cc_",i,sep="")
-#   m = max(cond_correlations$cor[cond_correlations$condition==i])*.7
-#   #d = cond_correlations[which(cond_correlations$condition==i & cond_correlations$cor>m),]
-#   print(cond_correlations[which(cond_correlations$condition==i & cond_correlations$cor>m),])
-#   plt = ggplot(data = cond_correlations[which(cond_correlations$condition==i & cond_correlations$cor>m),], aes(x=basis, y=cor)) + 
-#      geom_bar(stat="identity")+
-#      facet_grid(~condition)+
-#      theme(axis.text.x = element_text(angle=60, hjust=1))
-#   print(plt)
-# }
-# library(dplyr)
-# cc <- cond_correlations %>%
-#  
-#                     group_by(condition) %>%
-#                     arrange(condition,desc(cor)) %>%
-#                     top_n(n=10, wt=cor)
-# 
-# 
-# plt = ggplot(cc,aes(basis,cor, fill=as.factor(condition)))+
-#   geom_bar(stat="identity")+
-#   facet_grid(~condition, scales="free",space="free")+
-#   
-#   theme(axis.text.x = element_text(angle=60, hjust=1))
-# 
-# plt
-# ggsave("Correlation_by_Condition.pdf", plt, width=10, height=4)
-# 
-# maxes = list()
-# max_ks = list()
-# 
-# for (i in 0:100){
-#   #initialize the correlations table
-#   correlations = data.frame(Basis=character(),Correlation=double(),Kendall=double())
-#   faceted_cors = NULL
-#   #we need to test if our model is doing any better than random...
-#   #let's scramble! (shuffles by condition and generalization)
-#   scrambled <- human %>%
-#     group_by(condition,generalization) %>%
-#     mutate(probability=sample(probability))
-#   
-#   #now compare all the model bases to this scramble!
-#   for(f in list.files("ag/", full.names=TRUE)) {
-#    
-#       ag <- read.csv(f, header=TRUE)
-#       
-#       
-#       # what if our probability was proportional to the runtime
-#       
-#       
-#         #saving the correlations to a dataframe
-#         c = cor.test(scrambled$probability,ag$modprobability)
-#         k = cor.test(scrambled$probability,ag$modprobability,method="kendall")
-#         
-#         correlations <-rbind(correlations,data.frame(Basis=name,Correlation=c$estimate, Kendall=k$estimate))
-#         
-#         #what about faceted correlations? 
-#         #if a model prediction is overall kind of crappy because it missed a single condition, 
-#         #we still want to see how well it did!
-#         
-#         
-#         #let's see the correlations for each condition (we could do by generalization also, but then we are dealing with very few points)
-#         cors <- ddply(na.omit(all), c("condition"), summarise, cor = round(cor(probability, modprobability), 2))
-#         cors$basis = name
-#         
-#         #put together the correlations by condition
-#         faceted_cors = rbind(faceted_cors,cors)
-# 
-#   }
-#   
-#   m = max(correlations$Correlation)
-#   mk = max(correlations$Kendall)
-#   maxes<-append(maxes,m)
-#   max_ks <-append(max_ks,mk)
-#       
-# }
-# 
-# mc = read.csv("MaxCorrelations.csv")
-# plt = ggplot(mc, aes(x=m)) + geom_density()
-# 
-# mc$Better = FALSE
-# mc$Better[mc$m > max(correlations$Correlation)] = TRUE
-# 
-# num = length(mc$Better[mc$Better==TRUE]) #number of better than REAL human max correlation
-# denom = length(mc$Better[mc$Better==FALSE] )#number of worse than REAL human max correlation
-# 
-# num/length(mc$Better)
-# #0.0990099
-# 
-# 
-# mk = read.csv("MaximumKendalls.csv")
-# plt = ggplot(mk, aes(x=kk)) + geom_density()
-# plt
-# mk$Better = FALSE
-# mk$Better[mk$kk >= max(correlations$Kendall)] = TRUE
-# 
-# numk = length(mk$Better[mk$Better==TRUE]) #number of better than REAL human max correlation
-# denomk = length(mk$Better[mk$Better==FALSE] )#number of worse than REAL human max correlation
-# 
-# numk/length(mk$Better)
-# #.03960396
-# plt = ggplot(correlations, aes(x=Correlation)) + geom_density()
-# plt
-# 
-# 
-# plt = ggplot(correlations, aes(x=Kendall)) + geom_density()
-# plt
-# 
-# humplt = ggplot(scrambled, aes(answer, probability, fill=as.factor(condition)))+
-#   geom_bar(stat="identity")+
-#   facet_grid(~generalization~condition)
-# humplt
+# Now a histogram of maxes will show what we want
+plt <- ggplot(maxes, aes(x=ll)) +
+        geom_histogram() +
+        geom_vline(xintercept=max(D$ll), col="red", size=2) # draw a line at the real max
+
+# We could plot the others but we probably want to plot the cor of the max ll, not the max cor, right?
+# In that case we need to change above where maxes is defined
