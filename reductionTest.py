6749b4f750c2092da5612609c5e4521681ad4080
JennaRegister <jregiste@u.rochester.edu>
2017-03-16 09:42:05 -0400

    fixed reduction unit tests

--- reductionTest.py
+++ reductionTest.py
@@ -4,7 +4,7 @@ from reduction import reduce_combinator
 class ReductionTest(unittest.TestCase):
     def runTest(self):
         #dictionary tests contains combinator strings (key) and expected reduction (value)
-        tests = {'.....SKKKxy':'x','..Kxy':'x','....E.....SKKKxy..Kxyab':'a','....E.....KxySxyzab':'b','....E.......KxySxyzKxySxyz':'Sxyz','..Txy':'yx','.Mx':'xx','...Bxyz':'.x.yz','...Sxyz':'..xz.yz','..Wxy':'xyy','.Ix':'x'}
+        tests = {'.....SKKKxy':'x','..Kxy':'x','....E.....SKKKxy..Kxyab':'a','....E.....KxySxyzab':'b','....E.......KxySxyzKxySxyz':'Sxyz','..Txy':'.yx','.Mx':'.xx','...Bxyz':'.x.yz','...Sxyz':'..xz.yz','..Wxy':'.xyy','.Ix':'x'}
         print "# Testing reduction"
         for k,v in tests.iteritems():
             #assert that reduce_combinator returns the expected value
