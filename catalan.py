18ce22484fcbdbcf489b40fb890c44268214edec
Jenna Register <jennaregister@Jennas-MacBook-Air.local>
2017-12-05 15:11:46 -0500

    adding a prior on solutions

---
+++ catalan.py
@@ -0,0 +1,21 @@
+def catalan(n):
+    # Base Case
+    if n <=1 :
+        return 1
+
+    # Catalan(n) is the sum of catalan(i)*catalan(n-i-1)
+    res = 0
+    for i in range(n):
+        res += catalan(i) * catalan(n-i-1)
+
+    return res
+
+f = open('catalan.txt', 'w+')
+
+print "Computing the Catalan numbers..."
+# Driver Program to test above function
+
+for i in range(20):
+    print>> f, str(catalan(i)) +",",
+
+print "complete!"
